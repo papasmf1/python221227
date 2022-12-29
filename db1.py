aebbe963cb08ee3ec00e2f07aa136c429d563450
papasmf1 <papasmf1@gmail.com>
2022-12-29 14:19:56 +0900

    크롤링 연습1

--- db1.py
+++ db1.py
@@ -1,7 +1,8 @@
 # db1.py
 import sqlite3
 #1)연결객체(임시로 메모리에서 작업)
-con = sqlite3.connect(":memory:")
+#물리적인 파일로 저장
+con = sqlite3.connect("c:\\work\\sample.db")
 #2)커서객체(실제 SQL구문을 실행하고 결과 다루기)
 cur = con.cursor()
 #테이블(데이터 구조 만들기):ANSI SQL 92, 99
@@ -12,9 +13,13 @@ cur.execute("insert into PhoneBook values ('derick','010-123');")
 name = "gildong"
 phoneNumber = "010-123"
 cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber) )
-
+#여러건을 입력(2차원 배열을 행렬로 저장)
+datalist = (("tom","010-111"), ("dsp","010-333"))
+cur.executemany("insert into PhoneBook values (?, ?);", datalist )
 #검색
 cur.execute("select * from PhoneBook;")
 for row in cur:
     print(row)
 
+#마지막에 정상적으로 완료(쓰기작업)
+con.commit() 
