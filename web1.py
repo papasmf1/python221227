0f34af5ebf50f347f8d7e48c9863b913cf770c4b
papasmf1 <papasmf1@gmail.com>
2022-12-29 14:53:58 +0900

    크롤링 연습2

---
+++ web1.py
@@ -0,0 +1,17 @@
+# web1.py 
+#웹에 데이터를 수집 
+from bs4 import BeautifulSoup
+
+#웹페이지를 로딩(함수나 메서드를 연속으로 부르는 메서드체인)
+page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read() 
+#검색을 할 객체(xml문서, xml) 
+soup = BeautifulSoup(page, "html.parser")
+#전체를 보기 
+#print( soup.prettify() )
+#문서 <p> 몽땅 검색 
+#print( soup.find_all("p") )
+#첫번째 <p>만 검색
+print( soup.find("p") )
+
+
+
