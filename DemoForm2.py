2dcf80ba3bee16cea0611cbbbaa5efaf1eeba78d
papasmf1 <papasmf1@gmail.com>
2022-12-30 11:15:57 +0900

    GUI 연습3

--- DemoForm2.py
+++ DemoForm2.py
@@ -3,6 +3,10 @@
 import sys 
 from PyQt5.QtWidgets import * 
 from PyQt5 import uic 
+#웹서버에 페이지 실행을 요청
+import urllib.request
+#크롤링
+from bs4 import BeautifulSoup
 
 #화면을 로딩(파일이름 변경)
 form_class = uic.loadUiType("DemoForm2.ui")[0]
@@ -15,7 +19,21 @@ class DemoForm(QMainWindow, form_class):
         self.setupUi(self)
         self.label.setText("첫번째 Qt데모~~")
     def firstClick(self):
-        self.label.setText("첫번째 버튼")
+        #파일로 저장
+        f = open(r"c:\work\webtoon.txt", "wt", encoding="utf-8")
+        #페이징처리
+        for i in range(1,6):
+            url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+            print( url )
+            data = urllib.request.urlopen(url)
+            soup = BeautifulSoup(data, "html.parser")
+            cartoons = soup.find_all("td", class_="title")
+            for item in cartoons:
+                title = item.find("a").text.strip()
+                print(title)
+                f.write(title + "\n")
+        f.close()
+        self.label.setText("네이버 웹툰 크롤링 종료~~")
     def secondClick(self):
         self.label.setText("두번째 버튼 클릭")
     def thirdClick(self):
